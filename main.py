ab74adc6d853f00238ad9c2de7dd96c9cf1a54dc
dyakin.i <iigor93@gmail.com>
2023-06-01 13:05:13 +0300

    index

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, Body
 
 from sql_app.database import engine, DB_SESSION
 
@@ -24,10 +24,11 @@ def get_user_profile(db: DB_SESSION, user_profile_id: int = 1):
     return user_profile
 
 
-@app.put("/user_profile/{user_profile_id}/", response_model=user_profile_schemas.UserProfile)
-def update_user_profile(db: DB_SESSION, user_profile: user_profile_schemas.UserProfile, user_profile_id: int = 1):
-    user_profile_updated = user_profile_crud.update_user_profile(db, user_profile)
-    return user_profile_updated
+@app.put("/user_profile/{user_profile_id}/")  #, response_model=user_profile_schemas.UserProfile)
+def update_user_profile(db: DB_SESSION, request: dict = Request):  # user_profile: user_profile_schemas.UserProfile, user_profile_id: int = 1):
+    print(request)
+    # user_profile_updated = user_profile_crud.update_user_profile(db, user_profile)
+    return "OK"  # user_profile_updated
 
 
 """ BOOKS """
