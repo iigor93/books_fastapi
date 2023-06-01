f72d29f66247b266830a5954759753f484c2e3f0
dyakin.i <iigor93@gmail.com>
2023-06-01 12:28:53 +0300

    index

--- user_profile/schemas.py
+++ user_profile/schemas.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class UserProfile(BaseModel):
     """ Pydantic model for request """
 
-    id: int
+    id: int = 1
     column_name: bool
     column_title: bool
     column_author: bool
