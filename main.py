a6e9d396589167999e8e65ecd409ded6561e5ec5
dyakin.i <iigor93@gmail.com>
2023-06-02 13:18:25 +0300

    index

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ def get_user_profile(db: DB_SESSION, user_profile_id: int = 1):
     return user_profile
 
 
-@app.put("/user_profile/{user_profile_id}/")  # , response_model=user_profile_schemas.UserProfile)
+@app.put("/user_profile/{user_profile_id}/", response_model=user_profile_schemas.UserProfile)
 async def update_user_profile(db: DB_SESSION, user_profile: user_profile_schemas.UserProfile, user_profile_id: int = 1):
     user_profile_updated = user_profile_crud.update_user_profile(db, user_profile)
     print("USER PROFILE UPDATED", user_profile_updated.column_name)
-    return "ok"  # , user_profile_updated
+    return user_profile_updated
 
 
 """ BOOKS """
