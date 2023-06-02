3a2591ec1df1566f25781d0e99c4585eec57d4e8
dyakin.i <iigor93@gmail.com>
2023-06-02 13:56:18 +0300

    index

--- books/schemas.py
+++ books/schemas.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
@@ -5,10 +7,10 @@ class Book(BaseModel):
     """ Pydantic model for request """
 
     id: int | None = None
-    name: str
-    title: str | None = None
+    name: str = Field(min_length=1, max_length=20)
+    title: Optional[str] = Field(None, max_length=30)
     author: str
-    description: str | None = None
+    description: Optional[str] = Field(None, max_length=512)
     price: int = Field(gt=0, le=99999)
 
     class Config:
